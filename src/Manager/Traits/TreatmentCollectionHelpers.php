bfbd5fbd658f863427d6fdd991d7afd1be96b70c
Rehan Manzoor <manzoorr@amazon.com>
2021-11-02 04:34:58 +0400

    Fix return type

--- src/Manager/Traits/TreatmentCollectionHelpers.php
+++ src/Manager/Traits/TreatmentCollectionHelpers.php
@@ -4,12 +4,12 @@ namespace ABLab\Accessor\Manager\Traits;
 
 trait TreatmentCollectionHelpers
 {
-    protected function getApplicationSetting($applications, $application): array
+    protected function getApplicationSetting($applications, $application): null|array
     {
         return collect($applications)->where('unique_id', $application)->first();
     }
 
-    protected function getLaunchedTreatment(array $applicationSetting)
+    protected function getLaunchedTreatment(array $applicationSetting): null|array
     {
         return collect($applicationSetting['treatments'])->where('allocation', '>=', 100)->first();
     }
